220596c4454eb08a4361f1bccbcc0a9645d561fa
Stephen Paul Weber <singpolyma@singpolyma.net>
2009-06-16 20:27:58 -0700

    Whitespace/style

--- mnencode.c
+++ mnencode.c
@@ -2,22 +2,21 @@
 #include <string.h>
 #include "mnemonic.h"
 
-int main ()
-{
-  mn_byte buf[0x10000];
-  char outbuf[0x90000];
-  int buflen;
-  int n;
+int main(void) {
+	mn_byte buf[0x10000];
+	char outbuf[0x90000];
+	int buflen;
+	int n;
 
-  fprintf (stderr, "%s\n", mn_wordlist_version);
+	fprintf (stderr, "%s\n", mn_wordlist_version);
 
-  buflen = fread (buf, 1, sizeof (buf), stdin);
-  n = mn_encode (buf, buflen, outbuf, sizeof outbuf, "x x x. x x x. x x x\n");
-  if (n == 0)
-    fwrite (outbuf, 1, strlen (outbuf), stdout);
-  else
-    fprintf (stderr, "mn_encode error %d\n", n);
-  putchar ('\n');
+	buflen = fread (buf, 1, sizeof (buf), stdin);
+	n = mn_encode (buf, buflen, outbuf, sizeof outbuf, "x x x. x x x. x x x\n");
+	if (n == 0)
+		fwrite (outbuf, 1, strlen (outbuf), stdout);
+	else
+		fprintf (stderr, "mn_encode error %d\n", n);
+	putchar ('\n');
 
-  return 0;
+	return 0;
 }
