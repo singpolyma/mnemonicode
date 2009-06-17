a5947c0ee0305ddbfd208e9ead7ed30b38df2152
Stephen Paul Weber <singpolyma@singpolyma.net>
2009-06-16 20:41:59 -0700

    More style changes

--- mndecode.c
+++ mndecode.c
@@ -7,14 +7,15 @@ int main(void) {
 	int buflen;
 	int n;
 
-	buflen = fread (buf, 1, sizeof buf - 1, stdin);
+	buflen = fread(buf, 1, sizeof buf - 1, stdin);
 	buf[buflen] = 0;
 
-	n = mn_decode (buf, outbuf, sizeof outbuf);
-	if (n < 0)
-		fprintf (stderr, "mn_decode result %d\n", n);
-	else 
-		fwrite (outbuf, 1, n, stdout);
+	n = mn_decode(buf, outbuf, sizeof outbuf);
+	if(n < 0) {
+		fprintf(stderr, "mn_decode result %d\n", n);
+	} else {
+		fwrite(outbuf, 1, n, stdout);
+	}
 
 	return 0;
 }
